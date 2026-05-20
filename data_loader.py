be3630c7378c9463afc3e16cb80008f598746b4b
Konte Harouma <ton@email.com>
2026-05-20 18:25:17 +0200

    feature: add data_loader

---
+++ data_loader.py
@@ -0,0 +1,28 @@
+import pandas
+
+
+
+def load_data(file_path, target_col="target"):
+	if ".csv" in file_path:
+		df = pandas.read_csv(file_path)
+
+	elif ".xlsx" in file_path:
+		df = pandas.read_excel(file_path)
+
+	else:
+		raise Exception("The file path must be a csv or excel file !")
+
+	if target_col not in df.columns:
+		raise Exception("The taget column does not exist")
+
+	print(df[target_col].value_counts())
+	X = df.drop(columns=[target_col])
+	Y = df[target_col]
+	return X, Y 
+
+
+
+
+
+
+
