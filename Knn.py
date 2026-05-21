58824db9268103c186f4a3ffadff0b7e5ed79160
Konte Harouma <ton@email.com>
2026-05-21 06:41:44 +0200

    feature: add KNN class with fit and predict

---
+++ Knn.py
@@ -0,0 +1,24 @@
+class KNN:
+    def __init__(self , K):
+        self.K = K
+        self.X_train= None
+        self.Y_train = None
+        pass
+    
+    def fit(self , X , Y):
+        self.X_train= X
+        self.Y_train = Y
+        pass
+    
+    def predict(self , X):
+        distance = [self._distance(X, x) for x in self.X_train]
+        k_nearest = sorted(range(len(distances)), key=lambda i: distances[i])[:self.K]
+        k_labels = [self.Y_train.iloc[i] for i in k_nearest]
+
+        return max(set(k_labels), key=k_labels.count)
+    
+    def evaluate(self):
+        pass
+    
+    def grid_search(self):
+        pass
