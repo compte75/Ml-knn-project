f46b5ce7cd47531656857270d8825e4baadec3d5
Konte Harouma <ton@email.com>
2026-05-21 06:55:17 +0200

    feature: add grid_search method

--- Knn.py
+++ Knn.py
@@ -20,9 +20,20 @@ class KNN:
     def evaluate(self, X_test , Y_test):
         predictions = [self.predict(X_test.iloc[i]) for i in range(len(X_test))]
         accuracy = sum(predictions[i] == Y_test.iloc[i] for i in range(len(Y_test))) / len(Y_test)
-        
+
         return accuracy
         
     
-    def grid_search(self):
-        pass
+    def grid_search(self, X_train, Y_train, X_test, Y_test, k_range=range(1, 20, 2)):
+        best_k = None
+        best_accuracy = 0
+        for k in k_range:
+            self.K = k
+        self.fit(X_train, Y_train)
+        accuracy = self.evaluate(X_test, Y_test)
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_k = k
+        self.K = best_k
+        return best_k, best_accuracy
+        
